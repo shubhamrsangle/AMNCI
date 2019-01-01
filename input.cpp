4a0153e1559b0288a61b00fabab731e8be843155
shubhamrsangle <36818228+shubhamrsangle@users.noreply.github.com>
2019-01-01 15:19:00 +0530

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+using namespace std;
+
+string abcd="(abcd)";
+vois shubham();
+abc=def();
+ghi();
+ghi2();
+void main(){
+
+	int a;
+	cin>>a;
+	cout<<"Input Number was "<<a<<endl;
+}
